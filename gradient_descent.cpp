601519808d9a2c6c0ea11cfa6d0eae8b3bdf1d61
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-15 04:19:47 +0530

    refactoring into matrices

--- gradient_descent.cpp
+++ gradient_descent.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-float produce_random(){
+//The first three functions are just incase somebody wants to test on random numbers first, with linear form for now
+
+float produce_random(){ // This is just producing random numbers, what order we need each element in, depends
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(0,99); // This will just produce the integers
@@ -16,6 +18,15 @@ int* produce_random_arr(int size){
         rand_arr[i] = produce_random();
     }
     return rand_arr;
+}
+
+int* produce_defined_output(int m, int c, int* input_arr, int size){ // m is slope, c is the intercept :)
+    int* y_axis = new int[size];
+
+    for(int i = 0; i<size; i++){
+        y_axis[i] = m*input_arr[i] + c;
+    }
+
 }
 
 int main(){
