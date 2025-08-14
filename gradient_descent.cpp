18600e3a0b1d97f7785d05a25f62490c4ced9b29
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-15 04:05:41 +0530

    code for producing random stuff added

--- gradient_descent.cpp
+++ gradient_descent.cpp
@@ -10,6 +10,17 @@ float produce_random(){
     return dis(gen);
 }
 
+int* produce_random_arr(int size){
+    int* rand_arr = new int[size];
+    for(int i =0; i<size; i++){
+        rand_arr[i] = produce_random();
+    }
+    return rand_arr;
+}
+
 int main(){
-    cout<<produce_random()<<'\n';
+    int* rand_arr = produce_random_arr(5);
+    for(int i =0; i<5; i++){
+        cout<<rand_arr[i]<<' ';
+    }
 }
