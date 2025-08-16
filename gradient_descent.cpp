303b6172ecbaaf481aba93e0e47d5230a5749648
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-16 13:37:22 +0530

    proper reshaping done of a flattened array

--- gradient_descent.cpp
+++ gradient_descent.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 //The first three functions are just incase somebody wants to test on random numbers first, with linear form for now
 
-float produce_random(){ // This is just producing random numbers, what order we need each element in, depends
+int produce_random(){ // This is just producing random numbers, what order we need each element in, depends
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(0,99); // This will just produce the integers
     return dis(gen);
 }
 
-int* produce_random_arr(int size){
-    int* rand_arr = new int[size];
+int** produce_random_arr(int size){ // perform reshape
+    int** rand_arr = new int*[size]; // We will now assign addresses :)
     for(int i =0; i<size; i++){
-        rand_arr[i] = produce_random();
+        rand_arr[i] = new int[1];
+        rand_arr[i][0] = produce_random(); // now we save actual addresses
     }
     return rand_arr;
 }
@@ -27,6 +28,11 @@ int** produce_random_arr_matrix(int rows, int cols){
     }
     return matrix;
 }
+
+int* matrix_mult(int rows, int cols){ // We still have rows and cols here, weight*x chalega, that's why this model for now
+
+}
+
 int* produce_defined_output(int m, int c, int* input_arr, int size){ // m is slope, c is the intercept :)
     int* y_axis = new int[size];
 
@@ -37,8 +43,5 @@ int* produce_defined_output(int m, int c, int* input_arr, int size){ // m is slo
 }
 
 int main(){
-    int* rand_arr = produce_random_arr(5);
-    for(int i =0; i<5; i++){
-        cout<<rand_arr[i]<<' ';
-    }
+   
 }
