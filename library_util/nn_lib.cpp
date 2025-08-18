ac1c5f32a1d56d584d275f13921358d6b0479f4d
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-18 13:51:25 +0530

    probably last commit till 5pm, better save the work

--- library_util/nn_lib.cpp
+++ library_util/nn_lib.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 class ActivationFunctions{
     private:
-        
+        double weight;
+        double bias;
+        double x_axis;
+
+    public:
+    ActivationFunctions(double weight, double bias){
+        this->weight = weight;
+        this->bias = bias;
+        this->x_axis = 0;
+    }
+
+    double compute_x_axis(double input){
+        return (input*this->weight)+this->bias;
+    }
+
+    double relu_function()
 }
