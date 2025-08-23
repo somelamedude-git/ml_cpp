97498c0ab2a23a47315054b4dff6a65767eb4935
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-23 17:02:34 +0530

    expansion for e added

--- math_expr.cpp
+++ math_expr.cpp
@@ -14,4 +14,9 @@ class MathExpansions{
 		double output = (x-1) - ((x-1)*(x-1))/2 + ((x-1)*(x-1)*(x-1))/3 - ((x-1)*(x-1)*(x-1)*(x-1))/4;
 		return output;
 	}
+
+	double eExpand(){
+		double output = 1 + x + (x*x)/2 + (x*x*x)/6 + (x*x*x*x)/24 + (x*x*x*x*x)/120;
+		return output;
+	}
 }
