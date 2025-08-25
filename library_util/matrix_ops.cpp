d9ade4dc2eccc123076b43c63d8be1d17018c5e6
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-25 13:22:18 +0530

    matrix_ops library fixed

--- library_util/matrix_ops.cpp
+++ library_util/matrix_ops.cpp
@@ -57,7 +57,7 @@ void matrix_multiplication(
     }
 }
 
-void convert_twoD(float single_D_mat[], int size, float two_D_mat[1][]){
+void convert_twoD(float single_D_mat[], int size, float two_D_mat[1][MAX_COL_ONE]){
 	for(int j =0; j<size; j++){
 		two_D_mat[0][j] = single_D_mat[j];
 	}
--- library_util/matrix_ops.h
+++ library_util/matrix_ops.h
@@ -8,4 +8,5 @@ unsigned short int** transpose(unsigned short int** matrix, int rows, int cols);
 void matrix_multiplication(unsigned short int matrix_one[][MAX_COL_ONE], unsigned short int matrix_two[][MAX_COL_TWO],
 		int product_matrix[][MAX_COL_TWO], int row_one, int col_one, int row_two, int col_two);
 
-void convert_twoD(float single_D_mat[], int size, float two_D_mat[1][]);
+void convert_twoD(float single_D_mat[], int size, float two_D_mat[1][MAX_COL_ONE]);
+#endif
