d28b09d676972477404f5eb5dabdc4e8a61bdd77
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-25 14:41:29 +0530

    forward prop function written

--- library_util/nn_lib.cpp
+++ library_util/nn_lib.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "math_expr.h"
+#include "matrix_ops.h"
+
+#define MAX_COL_ONE 100
 
 using namespace std;
 
@@ -20,10 +23,33 @@ class ActivationFunctions{
         this->x_axis= (input*this->weight)+this->bias;
     }
 
-    double softplus(){
+    double softplus(){ // To find activation
         MathExpansions m1(this->x_axis);
         double softplus_val = MathExpansions(1 + m1.eExpand()).logarithm();
 
 	return softplus_val;
     }
+
+    float[] layer_calc_forwardprop(int size_next, int size_current, float weights[][MAX_COL_ONE], float biases[], float activation[], float resultant[1][MAX_COL_ONE]){
+	   // Weights are going to have dimensions [size_current][size_next]
+	   // biases are going to have dimensions [size_next]
+	   // activation -> [size_current]
+
+	    float two_D_mat[1][MAX_COL_ONE];
+	    convert_twoD(activation, size_current, two_D_mat);
+	    float product_matrix[1][size_next];
+
+	    matrix_multiplication(two_D_mat, weights, product_matrix, 1, size_current, size_next, size_current);
+
+	    for(int j =0; j<size_next; j++){
+		    product_matrix[0][j] = product_matrix[0][j]+biases[j];
+		    resultant[0][j] = softplus(product_matrix[0][j]);
+	    }
+    }
+
+
+
+
+
+
 };
