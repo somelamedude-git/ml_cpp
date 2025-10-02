598962d7429bcaa603f97a9757afd9d4e219f893
somelamedude-git <cozydeveloper843@gmail.com>
2025-10-03 01:16:49 +0530

    activation function applied on lists

--- nn_full/nn.c
+++ nn_full/nn.c
@@ -22,19 +22,31 @@ Network initialize_network(int num_layers, int[] neuron_per_layer){
 	return net;
 }
 
-double sigmoid(double x){
-	double sig = 1.0/(1.0 + exp(-x));
-	return sig;
+void sigmoid(double x, double sig){
+	sig = 1.0/(1.0 + exp(-x));
 }
 
-double sigmoid_derivative(double x){
+void sigmoid_derivative(double x, double derivative){
 	double derivative = exp(-x)/((1+exp(-x))*(1+exp(-x)));
 
 	return derivative;
 }
 
+void sigmoid_list(int x_list[], int size_of_list, int activated_array[]){
+	for(int i =0; i<size_of_list; i++){
+		int x = 0;
+		sigmoid(x_list[i], x);
+		activated_array[i] = x;
+	}
+}
 
-
+void sigmoid_derivative_list(int x_list[], int size_of_list, int derivative_array[]){
+	for(int i =0; i<size_of_list; i++){
+		int x =0;
+		sigmoid_derivative(x_list[i], x);
+		derivative_array[i] = x;
+	}
+}
 
 
 
