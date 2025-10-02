5d1d9d12737de621c8eaa444f032a37eb26bd1de
somelamedude-git <cozydeveloper843@gmail.com>
2025-10-03 00:58:22 +0530

    sigmoid function added

--- nn_full/nn.c
+++ nn_full/nn.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <math.h>
 
 #define SIZE 10
 #define MAX_NUM_LAYERS 4
@@ -13,13 +14,26 @@ typedef struct Network{
 	int biases[MAX_NUM_LAYERS-1][SIZE][1];
 } Network;
 
-void initialize_network(int num_layers, int[] neuron_per_layer){
+Network initialize_network(int num_layers, int[] neuron_per_layer){
 	Network net = {0};
 	net.num_of_layers = num_layers;
 	net.neuron_per_layer = neuron_per_layer;
 
 	return net;
 }
+
+double sigmoid(double x){
+	double sig = 1.0/(1.0 + exp(-x));
+	return sig;
+}
+
+double sigmoid_derivative(double x){
+	double derivative = exp(-x)/((1+exp(-x))*(1+exp(-x)));
+
+	return derivative;
+}
+
+
 
 
 
